2b2e4159859319463fc999564a3aad98dce7c262
PIYUSH-69 <dalvi.piyu@gmail.com>
2023-12-30 23:42:23 +0530

    INITIAL

---
+++ app/src/main/java/com/example/nfc/splashscreen.kt
@@ -0,0 +1,11 @@
+package com.example.nfc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
