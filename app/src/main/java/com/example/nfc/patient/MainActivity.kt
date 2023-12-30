bf159bc064f6afd4b4d6faf36493394d1304da99
PIYUSH-69 <dalvi.piyu@gmail.com>
2023-12-31 00:26:25 +0530

    ARCHITECTURE

--- app/src/main/java/com/example/nfc/patient/MainActivity.kt
+++ app/src/main/java/com/example/nfc/patient/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.nfc
+package com.example.nfc.patient
 
 import android.os.Bundle
 import android.view.Menu
@@ -11,6 +11,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.example.nfc.R
 import com.example.nfc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
