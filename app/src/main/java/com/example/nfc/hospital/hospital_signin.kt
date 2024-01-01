f703d11d643920a66b33a566d53bfda481f70d54
PIYUSH-69 <dalvi.piyu@gmail.com>
2024-01-01 23:25:45 +0530

    ARCHITECTURE

---
+++ app/src/main/java/com/example/nfc/hospital/hospital_signin.kt
@@ -0,0 +1,12 @@
+package com.example.nfc.hospital
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nfc.R
+
+class hospital_signin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hospital_signin)
+    }
+}
---
+++ app/src/main/java/com/example/nfc/patient/patient_personal.kt
@@ -0,0 +1,12 @@
+package com.example.nfc.patient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nfc.R
+
+class patient_personal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_patient_personal)
+    }
+}
