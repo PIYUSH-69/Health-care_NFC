730fce6c9d512a28b894e5ee7b731f00f20bcde2
PIYUSH-69 <dalvi.piyu@gmail.com>
2024-01-01 23:25:33 +0530

    ARCHITECTURE

--- app/src/main/java/com/example/nfc/splashscreen.kt
+++ app/src/main/java/com/example/nfc/splashscreen.kt
@@ -1,11 +1,20 @@
 package com.example.nfc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
 
 class splashscreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splashscreen)
+
+        Handler(Looper.getMainLooper()).postDelayed(2000) {
+            finish()
+            startActivity(Intent(this, Register::class.java))
+        }
     }
 }
