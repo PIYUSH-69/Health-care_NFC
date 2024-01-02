7e23679d419ceea957bf7e21f9b271f4c7bd36a2
PIYUSH-69 <dalvi.piyu@gmail.com>
2024-01-02 22:35:48 +0530

    connecting the activity

--- app/src/main/java/com/example/nfc/hospital/hospital_signin.kt
+++ app/src/main/java/com/example/nfc/hospital/hospital_signin.kt
@@ -1,12 +1,22 @@
 package com.example.nfc.hospital
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.nfc.R
 
 class hospital_signin : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hospital_signin)
+
+        val log=findViewById<Button>(R.id.log2)
+        log.setOnClickListener {
+
+            startActivity(Intent(this,HOSPITAL::class.java))
+        }
     }
 }
