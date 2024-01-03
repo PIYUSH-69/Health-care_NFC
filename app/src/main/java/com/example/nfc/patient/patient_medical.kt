2669e4f54cfac65b0f9b7a40ca2b4968343ee5f1
PIYUSH-69 <dalvi.piyu@gmail.com>
2024-01-03 16:40:07 +0530

    patient_personal

--- app/src/main/java/com/example/nfc/patient/patient_medical.kt
+++ app/src/main/java/com/example/nfc/patient/patient_medical.kt
@@ -1,12 +1,20 @@
 package com.example.nfc.patient
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.nfc.R
 
 class patient_medical : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_medical)
+
+        val reg=findViewById<Button>(R.id.button)
+        reg.setOnClickListener {
+
+            startActivity(Intent(this,MainActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/example/nfc/patient/patient_personal.kt
+++ app/src/main/java/com/example/nfc/patient/patient_personal.kt
@@ -1,12 +1,46 @@
 package com.example.nfc.patient
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Button
 import com.example.nfc.R
+import com.google.android.material.textfield.TextInputEditText
 
 class patient_personal : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_personal)
+
+        val reg=findViewById<Button>(R.id.button3)
+        val firstn=findViewById<TextInputEditText>(R.id.fname)
+        val midn=findViewById<TextInputEditText>(R.id.mname)
+        val lname=findViewById<TextInputEditText>(R.id.lname)
+        val gender=findViewById<AutoCompleteTextView>(R.id.gender)
+        val dob=findViewById<TextInputEditText>(R.id.dob)
+        val phonenum=findViewById<TextInputEditText>(R.id.pnum)
+        val adharnum=findViewById<TextInputEditText>(R.id.anum)
+        val email=findViewById<TextInputEditText>(R.id.email)
+        val pass=findViewById<TextInputEditText>(R.id.pass)
+        val cpass=findViewById<TextInputEditText>(R.id.cpass)
+
+        val array= arrayListOf("Male","Female","Transgender")
+        val adapter=ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,array)
+        gender.setAdapter(adapter)
+
+
+
+        reg.setOnClickListener {
+
+
+
+            startActivity(Intent(this,patient_medical::class.java))
+
+        }
+
     }
 }
