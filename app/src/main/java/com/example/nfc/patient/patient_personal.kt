bcc3dc569642d5cf4885d8b6dcb413900cbf4a48
RVB179 <rajbhosle109@gmail.com>
2024-01-03 18:11:38 +0530

    small changes

--- app/src/main/java/com/example/nfc/patient/patient_personal.kt
+++ app/src/main/java/com/example/nfc/patient/patient_personal.kt
@@ -8,28 +8,34 @@ import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
 import com.example.nfc.R
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.textfield.TextInputEditText
+import java.util.Date
+import java.util.Locale
+import android.icu.text.SimpleDateFormat
+
 
 class patient_personal : AppCompatActivity() {
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_patient_personal)
 
-        val reg=findViewById<Button>(R.id.button3)
-        val firstn=findViewById<TextInputEditText>(R.id.fname)
-        val midn=findViewById<TextInputEditText>(R.id.mname)
-        val lname=findViewById<TextInputEditText>(R.id.lname)
-        val gender=findViewById<AutoCompleteTextView>(R.id.gender)
-        val dob=findViewById<TextInputEditText>(R.id.dob)
-        val phonenum=findViewById<TextInputEditText>(R.id.pnum)
-        val adharnum=findViewById<TextInputEditText>(R.id.anum)
-        val email=findViewById<TextInputEditText>(R.id.email)
-        val pass=findViewById<TextInputEditText>(R.id.pass)
-        val cpass=findViewById<TextInputEditText>(R.id.cpass)
+        val reg = findViewById<Button>(R.id.button3)
+        val firstn = findViewById<TextInputEditText>(R.id.fname)
+        val midn = findViewById<TextInputEditText>(R.id.mname)
+        val lname = findViewById<TextInputEditText>(R.id.lname)
+        val gender = findViewById<AutoCompleteTextView>(R.id.gender)
+        var dob = findViewById<TextInputEditText>(R.id.dob)
+        val phonenum = findViewById<TextInputEditText>(R.id.pnum)
+        val adharnum = findViewById<TextInputEditText>(R.id.anum)
+        val email = findViewById<TextInputEditText>(R.id.email)
+        val pass = findViewById<TextInputEditText>(R.id.pass)
+        val cpass = findViewById<TextInputEditText>(R.id.cpass)
 
-        val array= arrayListOf("Male","Female","Transgender")
-        val adapter=ArrayAdapter(this,android.R.layout.simple_spinner_dropdown_item,array)
+        val array = arrayListOf("Male", "Female", "Transgender")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, array)
         gender.setAdapter(adapter)
 
 
@@ -37,10 +43,29 @@ class patient_personal : AppCompatActivity() {
         reg.setOnClickListener {
 
 
-
-            startActivity(Intent(this,patient_medical::class.java))
+            startActivity(Intent(this, patient_medical::class.java))
 
         }
 
+        val datePickerBuilder = MaterialDatePicker.Builder.datePicker()
+        val datePicker = datePickerBuilder.build()
+
+
+        dob.setOnClickListener {
+            datePicker.show(supportFragmentManager, "DatePicker")
+        }
+
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            // Format the selected date from the Unix timestamp
+            val selectedDateInMillis = selection as Long
+            val formattedDate = formatDate(selectedDateInMillis)
+            dob.setText(formattedDate)
+        }
+
+    }
+
+    private fun formatDate(timestampInMillis: Long): String {
+        val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.US)
+        return sdf.format(Date(timestampInMillis))
     }
 }
