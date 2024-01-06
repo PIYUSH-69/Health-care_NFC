fbec65cc755d4036913407d371689375e07a44df
RVB179 <rajbhosle109@gmail.com>
2024-01-07 02:02:46 +0530

    minute improvisation

--- app/src/main/java/com/example/nfc/patient/patient_medical.kt
+++ app/src/main/java/com/example/nfc/patient/patient_medical.kt
@@ -27,7 +27,6 @@ class patient_medical : AppCompatActivity() {
         weight()
         bldgrp()
         ayush()
-        bldgrp()
 
         val reg=findViewById<Button>(R.id.button)
         reg.setOnClickListener {
