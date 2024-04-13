15b04e8bd4855c610f14865188e5ac00d5224239
Raj Bhosle <rajbhosle109@gmail.com>
2024-04-13 15:15:14 +0530

    changes

--- app/src/main/java/com/example/nfc/patient/sidenav/Records.kt
+++ app/src/main/java/com/example/nfc/patient/sidenav/Records.kt
@@ -6,16 +6,34 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.nfc.R
+import com.example.nfc.databinding.ActivityRecordsBinding
+import com.example.nfc.patient.patientcrud
+import com.example.nfc.patient.patientwrapper
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.runBlocking
 
 class Records : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRecordsBinding
+    private lateinit var patients: patientwrapper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_records)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityRecordsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val uid= Firebase.auth.currentUser?.uid.toString()
+
+        patients= runBlocking { patientcrud.getpatient(uid) }!!
+        binding.editTextHeight.setText(patients.HEIGHT)
+        binding.editTextWeight.setText(patients.WEIGHT)
+        binding.editTextBloodGroup.setText(patients.BLOOD_GROUP)
+        binding.editTextBloodPressure.setText(patients.BLOOD_PRESSURE)
+        binding.editTextDiabetes.setText(patients.DIABETES)
+        binding.editTextAsthama.setText(patients.ASTHAMA)
+        binding.editTextAllergies.setText(patients.Allergies)
+        binding.editTextSurgeries.setText(patients.SURGERIES)
     }
 }
